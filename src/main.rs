74d4485b028cb5feec3af6df114c1c699bfa401b
k-nasa <htilcs1115@gmail.com>
2019-11-29 13:44:03 +0900

    Add reading function that local git config

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn read_gitconfig() -> FprResult<(String, String)> {
+    let output = Command::new("git")
+        .arg("config")
+        .arg("--get")
+        .arg("remote.origin.url")
+        .output()
+        .expect("Failed run git config command");
+
+    let origin_url = std::str::from_utf8(&output.stdout)?.trim();
+
+    let owner = origin_url
+        .split('/')
+        .nth(3)
+        .ok_or_else(|| "Reading of origin url failed")?;
+
+    let repo = origin_url
+        .split('/')
+        .nth(4)
+        .ok_or_else(|| "Reading of origin url failed")?
+        .trim_end_matches(".git");
+
+    Ok((owner.to_owned(), repo.to_owned()))
+}
+
 fn build_app() -> App<'static, 'static> {
     App::new(crate_name!())
         .version(crate_version!())
